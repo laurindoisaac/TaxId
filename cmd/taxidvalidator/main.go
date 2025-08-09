1587ba5889d301ed525192fbf98d98b36a20cc75
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 00:21:19 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/taxidvalidator/main.go
@@ -0,0 +1,23 @@
+// cmd/taxidvalidator/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "taxidvalidator/internal/taxidvalidator"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := taxidvalidator.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
